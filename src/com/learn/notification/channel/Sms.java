9b508314dd817609bf6bb2a8a3d56fcf8e278973
Taide220643 <59428815+Taide220643@users.noreply.github.com>
2021-08-09 16:31:22 +0700

    Initial commit

---
+++ src/com/learn/notification/channel/Sms.java
@@ -0,0 +1,11 @@
+  
+package com.learn.notification.channel;
+
+import com.learn.notification.channel.interf.Notifiable;
+
+public class Sms implements Notifiable{
+	@Override
+	public void send() {
+		System.out.println("SEND SMS TO CUSTOMER");
+	}
+}
